a4f7c62b528699c88b96e73b24f05a1b92e61540
Sys Cools <syscools@gmail.com>
2017-04-19 16:30:38 +0000

    not so fast there

--- setup.sh
+++ setup.sh
@@ -5,5 +5,6 @@ cp dotvimrc /root/.vimrc
 cp dot_tmux.conf /root/.tmux.conf
 cp dotgitconfig /root/.gitconfig
 cp config /root/.ssh/
-git remote set-url origin git+ssh://github.com/syscools/commandline.git
+# not yet, since we have not setup our pub/priv key
+#git remote set-url origin git+ssh://github.com/syscools/commandline.git
 echo ". $(pwd)/mybashrc" >> ~/.bashrc
