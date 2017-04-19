51f5b2acd864418a2585a48d3ba6fcbb6cb8f043
Sys Cools <syscools@gmail.com>
2017-04-19 09:10:07 +0000

    optim

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 #
+mkdir -m 700 /root/.ssh
 cp dotvimrc /root/.vimrc
 cp dot_tmux.conf /root/.tmux.conf
 cp dotgitconfig /root/.gitconfig
 cp config /root/.ssh/
+git remote set-url origin git+ssh://github.com/syscools/commandline.git
 echo "source $(pwd)/mybashrc" >> ~/.bashrc
 source ~/.bashrc
