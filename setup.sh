49dc852a11948c3ed6280c37a001573faafe87da
syscools@gmail.com <syscools@gmail.com>
2017-04-26 15:45:18 +0000

    oeu

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ cp dotvimrc ~/.vimrc
 cp dot_tmux.conf ~/.tmux.conf
 cp dotgitconfig ~/.gitconfig
 cp config ~/.ssh/
-# this is a test
+# this is a test2
 # not yet, since we have not setup our pub/priv key
 #git remote set-url origin git+ssh://github.com/syscools/commandline.git
 echo ". $(pwd)/mybashrc" >> ~/.bashrc
